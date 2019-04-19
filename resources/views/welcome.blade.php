40086dd754f19a532495c723583d174b4c92a5d6
Shyam Sitaula <shyam.sitaula@oit.com.np>
2019-04-19 18:02:09 +0545

    Misc change to welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,14 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Opay
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="">Pay</a>
+                    <a href="">FAQ</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
